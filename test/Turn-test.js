79c16e8c988c2bcfe6e64ee2bd08c75f912b7594
Bruce Gordon <brc.gordon@gmail.com>
2020-10-05 17:57:46 -0600

    Create and pass tests for storing user guess and current Card

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
+const Card = require('../src/Card');
 
 describe('Turn', function() {
 
@@ -15,8 +16,29 @@ describe('Turn', function() {
     expect(turn).to.be.an.instanceof(Turn);
   });
 
+  it('should store a user guess', function() {
 
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
 
+    expect(turn.guess).to.equal('object');
+  });
+
+  it('should store an instantiation of Card', function() {
+
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+
+    expect(turn.card).to.equal(card);
+  });
+
+  it('should store a Card that is an object', function() {
+
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+
+    expect(turn.card).to.be.an('object');
+  });
 
 
 })
