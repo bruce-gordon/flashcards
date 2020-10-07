54f3b85fb09ed4b9ee00f0ef1c7005dfc478d3f3
Bruce Gordon <brc.gordon@gmail.com>
2020-10-07 17:14:13 -0600

    Create and test createCards() method in Game class

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,28 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
+const Round = require('../src/Round');
+const Deck = require('../src/Deck');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = 0,
+    this.cards = [],
+    this.deck = new Deck(this.cards)
+  }
+
+  createCards(data) {
+    for (var i = 0; i < data.length; i++) {
+      let card = new Card(data[i].id, data[i].question, data[i].answers, data[i].correctAnswer);
+      this.cards.push(card);
+    }
+  }
+
+  start(data) {
+    this.createCards(data);
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -13,6 +32,6 @@ class Game {
   printQuestion(round) {
       util.main(round);
   }
-}
+};
 
 module.exports = Game;
---
+++ test/Game-test.js
@@ -0,0 +1,61 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
+const Round = require('../src/Round');
+const Deck = require('../src/Deck');
+const Game = require('../src/Game');
+
+describe('Game', function() {
+  let game;
+  let data;
+
+
+  beforeEach(function() {
+    data = [{
+      'id': 1,
+      'question': "What is my cat's name?",
+      'answers': ['Kitty', 'Cat', 'Pepper'],
+      'correctAnswer': 'Pepper'
+    }, {
+      'id': 2,
+      'question': "What is the name of my state?",
+      'answers': ['Colorado', 'Maine', 'Hawaii'],
+      'correctAnswer': 'Colorado'
+    }, {
+      'id': 3,
+      'question': "What color is my car?",
+      'answers': ['Green', 'White', 'Gray'],
+      'correctAnswer': 'Gray'
+    }]
+
+    game = new Game();
+
+  });
+
+  it('should be a function', () => {
+    expect(Game).to.be.a('function');
+  });
+
+  it('should track the current round', () => {
+    expect(game.currentRound).to.equal(0);
+  });
+
+  it('should create cards from the data', () => {
+    game.createCards(data);
+    expect(game.cards[0]).to.deep.equal({id: 1, question: "What is my cat's name?", answers: ['Kitty', 'Cat', 'Pepper'], correctAnswer: "Pepper"});
+  });
+
+  it('should store all the cards', () => {
+    game.createCards(data);
+    expect(game.cards.length).to.equal(3);
+  });
+
+  it('should create a deck', () => {
+    game.createCards(data);
+    expect(game.deck.cards).to.be.an('array');
+    expect(game.deck).to.be.an('object');
+    expect(game.deck.countCards()).to.equal(3);
+  })
+});
